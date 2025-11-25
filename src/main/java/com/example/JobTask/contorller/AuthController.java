23182636e3b1569382ebd678d44fa74db1f95bc0
Mohammed Abdul Latif <mohammed.abdulatif@beshara.com>
2025-11-25 11:24:17 +0200

    finish Auth controller

---
+++ src/main/java/com/example/JobTask/contorller/AuthController.java
@@ -0,0 +1,73 @@
+package com.example.JobTask.contorller;
+
+import com.example.JobTask.dto.user.AuthenticationResponseDTO;
+import com.example.JobTask.dto.user.LoginRequestDTO;
+import com.example.JobTask.dto.user.RegisterRequestDTO;
+import com.example.JobTask.service.AuthService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public ResponseEntity<?>login(@Valid @RequestBody LoginRequestDTO loginDTO,
+                                     HttpServletResponse response){
+
+        AuthenticationResponseDTO authenticationResponseDTO = authService.login(loginDTO);
+
+        response.setHeader("Authorization","Bearer "+authenticationResponseDTO.getAccessToken());
+
+        Cookie refreshTokencookie = new Cookie("refreshToken",authenticationResponseDTO.getRefreshToken());
+
+        refreshTokencookie.setHttpOnly(true);
+        refreshTokencookie.setPath("/");
+        refreshTokencookie.setMaxAge(7*24*60*60);
+
+        response.addCookie(refreshTokencookie);
+
+        return ResponseEntity.ok().build();
+    }
+
+    public ResponseEntity<?>register(@Valid @RequestBody RegisterRequestDTO requestDTO){
+
+        authService.register(requestDTO);
+        return ResponseEntity.ok().build();
+    }
+
+    public ResponseEntity<?>refresh(HttpServletRequest request,
+                                    HttpServletResponse response){
+
+        Cookie[]cookies = request.getCookies();
+
+        String refreshToken = null;
+
+        for(Cookie cookie: cookies){
+
+            if("refreshToken".equals(cookie.getName())){
+                refreshToken = cookie.getValue();
+                break;
+            }
+        }
+
+        if(refreshToken == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        String newAccessToken = authService.refresh(refreshToken);
+
+        response.setHeader("Authorization","Bearer "+newAccessToken);
+        return ResponseEntity.ok().build();
+    }
+}
